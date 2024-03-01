4eeb0fa9537be42ddfede5d53bac6f70cab561ce
byiringiroaimefils <aimefils173@gmail.com>
2024-03-01 17:30:11 +0200

    V4

--- MERN Full Stack/FronEnd/Client/src/Components/Home.jsx
+++ MERN Full Stack/FronEnd/Client/src/Components/Home.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 import { Link } from "react-router-dom"
 import { AiOutlineEdit } from "react-icons/ai"
-import { BsInfoCircle } from "react-icons/bs"
+// import { BsInfoCircle } from "react-icons/bs"
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md"
 
 
@@ -22,68 +22,77 @@ export default function Home() {
                 console.log(error)
                 setLoading(false);
             })
-            
-        }, [])
-        console.log(Book)
-    return(
- 
+
+    }, [])
+    console.log(Book)
+    return (
+
         <div className="p-4">
-            <div className="flex justify-between items-center">
-                <h2 className="text-3xl my-8">Book List</h2>
-                <Link to="book/CreatBook">
-                    <MdOutlineAddBox className="text-2xl" />
+            <div className="flex justify-between justify-center  items-center">
+                <h2 className="text-3xl my-8 pl-5">Book List</h2>
+
+                <Link to="book/CreatBook" className="flex rounded  bg-sky-600 p-3 mr-20">
+                    <MdOutlineAddBox className="text-2xl " />
+                    <button className="">
+                        New
+                    </button>
                 </Link>
+
             </div>
             {
                 Loading ? (
-                    // <Spinner />
-                    <p>Loading....</p>
+                    <div className="ml-6">
+                        <p>Loading....</p>
+                        {/* // <Spinner /> */}
+                    </div>
                 ) : (
-                    <table>
-                        <thead>
-                            <tr>
-                                <th className="border border-slate-600  rounded-md">No</th>
-                                <th className="border border-slate-600  rounded-md">Title</th>
-                                <th className="border border-slate-600  rounded-md">Author</th>
-                                <th className="border border-slate-600  rounded-md">PublishYear</th>
-                                <th className="border border-slate-600  rounded-md">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Book.map((book, index) => (
-                                <tr key={book._id} className="h-8">
-                                    <td className="border border-slate-700 rounded-md text-center">
-                                        {index+1}
-                                    </td>
-                                    <td className="border border-slate-700 rounded-md text-center">
-                                        {book.Title}
-                                    </td>
-                                    <td className="border border-slate-700 rounded-md text-center">
-                                        {book.author}
-                                    </td>
-                                    <td className="border border-slate-700 rounded-md text-center">
-                                        {book.PublishYear}
-                                    </td>
-                                    <td>
-                                        <div className="flex justify-center gap-x-4">
-                                            <Link to={`book/detail/`}>
-                                                <BsInfoCircle className="text-2xl" />
+                    <div className="flex gap-5 p-5" >
+                        {Book.map((book, index) => (
+                            <div key={book._id} className=" border hover:border-sky-600 w-56 p-5 bg-slate-800  opacity-50 cursor-pointer ring-slate-200 shadow-slate-300">
+                                <Link to={`book/Detail/${book._id}`}>
+
+                                    <div className="">
+                                        <div>
+                                            <h6 className="pb-3">
+                                                {index + 1}
+                                            </h6>
+                                            <h4>
+                                                <h2 className="font-extrabold">TITLE</h2>
+                                                {book.Title}
+                                            </h4>
+                                            <h4>
+                                                <h2 className="font-extrabold">AUTHOR</h2>
+                                                {book.author}
+                                            </h4>
+                                            <p>
+                                                <h2 className="font-extrabold">PUBLISH YEAR</h2>
+                                                {book.PublishYear}
+                                            </p>
+                                        </div>
+
+                                        <div className="flex justify-center justify-between  pt-5">
+                                            {/* <Link to={`book/Detail/${book._id}`}>
+                                                {/* <BsInfoCircle className="text-2xl" /> */}
+                                            {/* </Link> */} 
+                                            <Link to={`/book/edit/${book._id}`} >
+                                                <AiOutlineEdit className="text-2xl hover:text-sky-600" />
                                             </Link>
-                                            <Link to={`/book/edit/${book.id}`}>
-                                                <AiOutlineEdit className="text-2xl" />
-                                            </Link>
-                                            <Link to={`/book/delete/${book.id}`}>
-                                                <MdOutlineDelete className="text-2xl" />
+                                            <Link to={`/book/delete/${book._id}`} >
+                                                <MdOutlineDelete className="text-2xl hover:text-red-600" />
                                             </Link>
                                         </div>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                                    </div>
+                                </Link>
+
+                            </div>
+
+                        ))}
+
+                    </div>
+
                 )
             }
-        </div>
+        </div >
     )
 }
 
